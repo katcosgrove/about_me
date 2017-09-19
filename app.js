7edf4c8dc2d4150d10ed830e0ac0e1f705242db0
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-09-19 14:38:11 -0700

    The first functional quiz question.

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+// OR is ||
+// and is &&
+
+//Introduction
+// var userName = prompt('Excelsior! What is your name?');
+//
+// alert('Hi, ' + userName + '! Nice to meet you. See if you can get the correct answer to the following questions about me.')
+
+//Question One
+var answer1 = prompt('Do I have any pets?').toLowerCase();
+if (answer1 === 'y' || answer1 === 'yes') {
+alert('Ding ding ding! You are correct! I have two munchkin cats.')
+} else {
+alert('BZZZZZT. Wrong! I have two munchkin cats.')
+}
+
+//Question Two
+//var answer2 = prompt('')

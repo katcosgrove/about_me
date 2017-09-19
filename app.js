647a46835c4505d3e7185a55e2bbb806963f8759
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-09-19 15:02:58 -0700

    Third question is now functional

--- app.js
+++ app.js
@@ -17,12 +17,19 @@ alert('BZZZZZT. Wrong! I have two munchkin cats.')
 }
 
 //Question Two
-var answer2 = prompt('Do you like bananas?').toLowerCase();
+var answer2 = prompt('Do I like bananas?').toLowerCase();
 console.log('Answer to question two was ' + answer2);
 if (answer2 === 'n' || answer2 === 'no'){
   alert('Correct! Hate them, with the one exception of a fried peanut butter and banana sandwich.')
 } else {
-  alert('Sorry! I hate them. I know it\s weird. :\(')
+  alert('Sorry! I hate them. I know it\'s weird. :\(')
 }
 
 //Question Three
+var answer3 = prompt('Have I ever lived in another country?')
+console.log('Answer to question three was ' + answer3);
+if (answer3 === 'y' || answer3 === 'yes'){
+  alert('You\'re right! I lived in Hungary for a few months earlier this year. Super tired of paprika.')
+} else {
+  alert('Bocsanat, nem! I lived in Hungary for a few months earlier this year. Super tired of paprika.')
+}

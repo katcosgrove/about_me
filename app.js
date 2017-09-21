bb7e770e603aef591a75bee325c9b6773bc6455e
Joel Clair <clair.joel@yahoo.com>
2017-09-21 14:21:46 -0700

    Class 4 coop coding functions

--- app.js
+++ app.js
@@ -1,59 +1,82 @@
 'use strict';
 var score = 0;
+var x = 0;
+var questions = ['Do I have any pets?', 'Do I like bananas?', 'Have I ever lived in another country?', 'Do I like movies?', 'Am I afraid of heights?']
+var correct = ['Ding ding ding! You are correct! I have two munchkin cats.', 'Correct! Hate them, with the one exception of a fried peanut butter and banana sandwich.', 'Igen! I lived in Hungary for a few months earlier this year. Super tired of paprika.', 'Maybe too much! I used to work for a large independent video store, assigned to the Horror department. I collect digitized copies of rare, out of print films, most of them foreign or within the horror genre.', 'Correct! At least, not as long as I\'m no more than thirty or so feed above ground or wearing a harness. Bouldering is one of my hobbies.']
+var incorrect = ['BZZZZZT. Wrong! I have two munchkin cats.', 'Sorry! I hate them. I know it\'s weird. :\(', 'Bocsanat, nem! I lived in Hungary for a few months earlier this year. Super tired of paprika.', 'Who doesn\'t like movies? Do you hate fun?', 'Nope! If I\'m wearing a harness or less than about thirty feet above ground, heights do not make me uncomfortable at all. Bouldering is one of my hobbies.']
 
 //Introduction
 var userName = prompt('Excelsior! What is your name?');
 alert('Hi, ' + userName + '! Nice to meet you. See if you can get the correct answer to the following questions about me.');
 
 //Question One
-var answer1 = prompt('Do I have any pets?').toLowerCase();
+function questionOne () {
+var answer1 = prompt(questions[x]).toLowerCase();
 console.log('Answer to question one was ' + answer1);
 if (answer1 === 'y' || answer1 === 'yes') {
   score++;
-  alert('Ding ding ding! You are correct! I have two munchkin cats.\nYour score is ' + score + '.');
+  alert(correct[x] + 'Your score is ' + score);
 } else {
-  alert('BZZZZZT. Wrong! I have two munchkin cats.\nYour score is ' + score + '.');
+  alert(incorrect[x] + 'Your score is ' + score);
+}
+x++;
 }
 
 //Question Two
-var answer2 = prompt('Do I like bananas?').toLowerCase();
+function questionTwo () {
+var answer2 = prompt(questions[x]).toLowerCase();
 console.log('Answer to question two was ' + answer2);
 if (answer2 === 'n' || answer2 === 'no'){
   score++;
-  alert('Correct! Hate them, with the one exception of a fried peanut butter and banana sandwich.\nYour score is ' + score + '.');
+  alert(correct[x] + 'Your score is ' + score);
 } else {
-  alert('Sorry! I hate them. I know it\'s weird. :\(\nYour score is ' + score + '.');
+  alert(incorrect[x] + 'Your score is ' + score);
+}
+x++;
 }
 
 //Question Three
-var answer3 = prompt('Have I ever lived in another country?').toLowerCase();
+function questionThree () {
+var answer3 = prompt(questions[x]).toLowerCase();
 console.log('Answer to question three was ' + answer3);
 if (answer3 === 'y' || answer3 === 'yes'){
   score++;
-  alert('Igen! I lived in Hungary for a few months earlier this year. Super tired of paprika.\nYour score is ' + score + '.');
+  alert(correct[x] + 'Your score is ' + score);
 } else {
-  alert('Bocsanat, nem! I lived in Hungary for a few months earlier this year. Super tired of paprika.\nYour score is ' + score + '.');
+  alert(incorrect[x] + 'Your score is ' + score);
 }
-
+x++;
+}
 //Question Four
-var answer4 = prompt('Do I like movies?').toLowerCase();
+function questionFour () {
+var answer4 = prompt(questions[x]).toLowerCase();
 console.log('Answer to question four was ' + answer4);
 if (answer4 === 'y' || answer4 === 'yes'){
   score++;
-  alert('Maybe too much! I used to work for a large independent video store, assigned to the Horror department. I collect digitized copies of rare, out of print films, most of them foreign or within the horror genre.\nYour score is ' + score + '.');
+  alert(correct[x] + 'Your score is ' + score);
 } else {
-  alert('Who doesn\'t like movies? Do you hate fun?\nYour score is ' + score + '.');
+  alert(incorrect[x] + 'Your score is ' + score);
 }
-
+x++;
+}
 // Question Five
-var answer5 = prompt('Am I afraid of heights?').toLowerCase();
+function questionFive () {
+var answer5 = prompt(questions[x]).toLowerCase();
 console.log('Answer to question five was ' + answer5);
 if (answer5 === 'n' || answer5 === 'no'){
   score++;
-  alert('Correct! At least, not as long as I\'m no more than thirty or so feed above ground or wearing a harness. Bouldering is one of my hobbies.\nYour score is ' + score + '.');
+  alert(correct[x] + 'Your score is ' + score);
 } else {
-  alert('Nope! If I\'m wearing a harness or less than about thirty feet above ground, heights do not make me uncomfortable at all. Bouldering is one of my hobbies.\nYour score is ' + score + '.');
+  alert(incorrect[x] + 'Your score is ' + score);
 }
+x++;
+}
+
+questionOne();
+questionTwo();
+questionThree();
+questionFour();
+questionFive();
 
 //Question Six
 var answer6 = 0;
